024eb971d06893bc4db29791be4a0ce21258aae4
t2hk <tthk.inoue@gmail.com>
2021-08-13 21:52:19 +0900

    change config file type js to json

--- config/env.production.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  test_message: 'This is prod test message.',
-  api_message: 'This is prod message from API.',
-  API_KEY: 'something_api_key_for_production.'
-}
--- config/env.staging.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  test_message: 'This is staging test message.',
-  api_message: 'This is staging message from API.',
-  API_KEY: 'something_api_key_for_staging.'
-}
